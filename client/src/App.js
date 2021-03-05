9f96c2f5f06c3c3f41d3c215d05b0622fec6317b
Eugene Ignatov <ignatovyevgen@gmail.com>
2021-03-05 14:24:23 -0600

    set up

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>Initial setup formulator</h1>
+      <h2>Biggs/Ignatov</h2>
     </div>
   );
 }
