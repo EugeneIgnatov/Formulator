e3b73b4a572d79877b85ec0336c6b6c8bc51b214
Eugene Ignatov <ignatovyevgen@gmail.com>
2021-03-05 18:52:03 -0600

    style

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,94 @@
-import './App.css';
+import './App.scss';
+import { useState } from 'react';
+
+import data from './data';
 
 function App() {
+  const [formula, setFormula] = useState('');
   return (
     <div className='App'>
-      <h1>Initial setup formulator</h1>
-      <h2>Biggs/Ignatov</h2>
+      {/* <h1 className='header'>Formulator</h1> */}
+      <header>
+        <form>
+          <div className='header_select'>
+            <label htmlFor='formulaName'>Formula</label>
+            <select
+              value={formula}
+              name='furmulaName'
+              id='formulaName'
+              onChange={(e) => setFormula(e.target.value)}>
+              <option name='Elecare Infant'>Elecare infant</option>
+              <option name='Elecare Jr.(unflavored/vanila)'>
+                Elecare Jr. (unflavored/vanila)
+              </option>
+            </select>
+          </div>
+          <div className='header_input'>
+            <label htmlFor='kcal/oz'>KCAL/ oz.</label>
+            <input placeholder='0'></input>
+          </div>
+          <div className='header_input'>
+            <label htmlFor='scoops'>#Scoops</label>
+            <input placeholder='0'></input>
+          </div>
+        </form>
+      </header>
+      <section className='section'>
+        <div>
+          <h2 className='section_data-header'>Data</h2>
+          <table>
+            <tbody>
+              <tr>
+                <th>kcal/scp</th>
+                <th>kcal/g</th>
+                <th>g pro/ g pwd</th>
+                <th>g pwd/scp</th>
+                <th>g pwd/lvl cup</th>
+              </tr>
+              <tr>
+                <td>44.7</td>
+                <td>4.75</td>
+                <td>0.15</td>
+                <td>9.4</td>
+                <td>107</td>
+              </tr>
+              <tr>
+                <td>7.2</td>
+                <td>2.2</td>
+                <td>0.74</td>
+                <td>400</td>
+                <td>14.1</td>
+              </tr>
+              <tr>
+                <th>g pwd/Tbsp</th>
+                <th>g pwd/tsp</th>
+                <th>disp (ml/g)</th>
+                <th>g pwd/can</th>
+                <th>oz pwd/can</th>
+              </tr>
+            </tbody>
+          </table>
+          <hr></hr>
+          <div>
+            Disp (ml/ scp)
+            <p>6.54</p>
+          </div>
+          <div>
+            Formula Total Vol. (oz)
+            <p>2.02</p>
+          </div>
+          <div>
+            Total Cals/ Formula
+            <p>44.5</p>
+          </div>
+        </div>
+
+        <div>
+          <h2 className='section_water-header'>Water</h2>
+          <p className='section_water-ml'>53.28ml</p>
+          <p className='section_water-oz'>1.80 oz</p>
+        </div>
+      </section>
     </div>
   );
 }
---
+++ client/src/data.js
@@ -0,0 +1,29 @@
+const data = [
+  {
+    name: 'Elecare Infant',
+    kcal_Scp: 44.7,
+    kcal_g: 4.75,
+    g_prot_g_poweder: 0.15,
+    g_poweder_scp: 9.4,
+    g_poweder_level_cup: 107.0,
+    g_poweder_tbls: 7.2,
+    g_poweder_tsp: 2.2,
+    disp: 0.74,
+    g_poweder_can: 400,
+    oz_lbs_powder_can: 14.1,
+  },
+  {
+    name: 'Elecare Jr.(unflavored/vanila)',
+    kcal_Scp: 44.6,
+    kcal_g: 4.69,
+    g_prot_g_poweder: 0.14,
+    g_poweder_scp: 9.5,
+    g_poweder_level_cup: 107.0,
+    g_poweder_tbls: 7.0,
+    g_poweder_tsp: 2.2,
+    disp: 0.74,
+    g_poweder_can: 400,
+    oz_lbs_powder_can: 14.1,
+  },
+];
+export default data;
