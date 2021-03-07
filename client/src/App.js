772097ffb52eb8c1855e853bf12fe5435fb18341
Eugene Ignatov <ignatovyevgen@gmail.com>
2021-03-06 20:45:13 -0600

    style desctop

--- client/src/App.js
+++ client/src/App.js
@@ -34,7 +34,7 @@ function App() {
         </form>
       </header>
       <section className='section'>
-        <div>
+        <div className='section_data'>
           <h2 className='section_data-header'>Data</h2>
           <table>
             <tbody>
@@ -45,18 +45,22 @@ function App() {
                 <th>g pwd/scp</th>
                 <th>g pwd/lvl cup</th>
               </tr>
-              <tr>
-                <td>44.7</td>
-                <td>4.75</td>
-                <td>0.15</td>
-                <td>9.4</td>
+              <tr className='table-data-row-top'>
+                <td className=' table-data-cell-right'>44.7</td>
+                <td className=' table-data-cell-right'>4.75</td>
+                <td className=' table-data-cell-right'>0.15</td>
+                <td className=' table-data-cell-right'>9.4</td>
                 <td>107</td>
               </tr>
-              <tr>
-                <td>7.2</td>
-                <td>2.2</td>
-                <td>0.74</td>
-                <td>400</td>
+              <tr className='table-data-row-bottom'>
+                <td className='table-data-cell-right'>7.2</td>
+                <td className='table-data-cell-right'>2.2</td>
+                <td
+                  className='
+                  table-data-cell-right'>
+                  0.74
+                </td>
+                <td className='table-data-cell-right'>400</td>
                 <td>14.1</td>
               </tr>
               <tr>
@@ -69,22 +73,23 @@ function App() {
             </tbody>
           </table>
           <hr></hr>
-          <div>
-            Disp (ml/ scp)
-            <p>6.54</p>
-          </div>
-          <div>
-            Formula Total Vol. (oz)
-            <p>2.02</p>
-          </div>
-          <div>
-            Total Cals/ Formula
-            <p>44.5</p>
+          <div className='section-footer'>
+            <div>
+              Disp (ml/ scp)
+              <p>6.54</p>
+            </div>
+            <div>
+              Formula Total Vol. (oz)
+              <p>2.02</p>
+            </div>
+            <div>
+              Total Cals/ Formula
+              <p>44.5</p>
+            </div>
           </div>
         </div>
-
-        <div>
-          <h2 className='section_water-header'>Water</h2>
+        <div className='section_water-div'>
+          <h2>Water</h2>
           <p className='section_water-ml'>53.28ml</p>
           <p className='section_water-oz'>1.80 oz</p>
         </div>
